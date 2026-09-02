76f724e3e5e50a3480880cce38ea06826cad260e
samiksha7094gupta-alt <samiksha7094gupta-alt>
2026-09-02 20:24:55 +0530

    js datatypes

--- Day/Day8/P3.js
+++ Day/Day8/P3.js
@@ -1,47 +1,50 @@
-// JS datatypes
-// Primitive data types:
-// 1. String
-// 2. Number
-// 3. Boolean
-// 4. Undefined
-// 5. Null
-// 6. Symbol
-// 7. BigInt
-// Non-primitive data types:
-// 1. Object
-// 2. Array
-// 3. Function
+// JS Datatypes
+// Primitive Data Type:
+    // 1. String
+    // 2. Number
+    // 3. Bigint
+    // 4. Boolean
+    // 5. Undefined
+    // 6. Null
+    // 7. Symbol
+// Non - Primitive Data types:
+    // 1. Object
+    // 2. Array
+    // 3. Function
 
-// primitive data type
-//let name = "Samiksha"; // string
-//let age = 20; // number
-//let bigIntNum = 12345678901234567890123; // BigInt
-//let isStudent = true; // boolean
-//let email = undefined; // undefined
-//let address = null; // null
-//let id = Symbol("id"); // symbol
+// Primitive Data Type
+    // 1. String
+    let name = "Rakesh"; 
+    // 2. Number
+    let age = 22; 
+    // 3. Bigint
+    let bigNumber = 1234567876543;
+    // 4. Boolean
+    let isActive = true;
+    // 5. Undefined
+    let value;
+    // 6. Null
+    let result = null;
+    // 7. Symbol
+    let id = Symbol("id");
 
-//console.log(typeof name); // string
-//console.log(typeof age); // number
-//console.log(typeof bigIntNum); // bigint
-//console.log(typeof isStudent); // boolean
-//console.log(typeof email); // undefined
-//console.log(typeof address); // object
-//console.log(typeof id); // symbol
-
-// Non-primitive data types:
-// 1. Object
-let person = {
-    name: "Samiksha",
-    age: 20,
-    isStudent: true
-};
-console.log(typeof person); // object
-// 2. Array
-let colors = ["red", "green", "blue"];
-console.log(typeof colors); // object
-// 3. Function
-let greet = function() {
-    console.log("Hello, World!");
-};
-console.log(typeof greet); // function
+    console.log(typeof name);
+    console.log(typeof age);
+    console.log(typeof bigNumber);
+    console.log(typeof isActive);
+    console.log(typeof value);
+    console.log(typeof result);
+    console.log(typeof id);
+   
+// Non - Primitive Data types:
+    // 1. Object
+    let person = {
+        name: "Rakesh",
+        age: 36
+    };
+    // 2. Array
+    let fruits = ["apple","mango","Banana"]
+    // 3. Function
+    function greet() {
+        return "Hello";
+    }
