d4f2c6f880e9b2d64fa5667eab9539c568de7230
samiksha7094gupta-alt <samiksha7094gupta-alt>
2026-09-02 21:38:39 +0530

    React

---
+++ Day/Day13/day-13-react-app/src/App.jsx
@@ -0,0 +1,33 @@
+import { Welcome } from "./components/P1";
+import { ProductCard } from "./components/P2";
+import headphones from "./assets/sony.jpg";
+import ButtonDemo from "./components/P3";
+import Counter from "./components/StateExampleOne";
+import ToggleText from "./components/StateExampleTwo";
+import NameInput from "./components/StateExampleThree";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <NameInput/>
+      <NameInput/>
+      <ToggleText />
+
+      <Counter />
+
+      <Welcome name="Samiksha" />
+
+      <ButtonDemo />
+
+      <ProductCard
+        //  name="Sony ULT Wear Noise Cancellation Wireless Bluetooth Over Ear Headphones with Massive Bass"
+        price={15999}
+        //  image="https://m.media-amazon.com/images/I/610DB8Cwm7L._SY450_.jpg"
+        image={headphones}
+      />
+    </>
+  );
+}
+
+export default App;
